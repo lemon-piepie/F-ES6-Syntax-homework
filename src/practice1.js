519ce9bed4512e7d7414087b465152ae183ee901
kevindongzg <dongkevin94@gmail.com>
2020-09-22 20:46:09 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,9 +1,13 @@
+// TODO 可以思考一下可否用reduce来做
 const parseData = (input) => {
+    // TODO 解构可以优化到函数接受参数中
     let {data , column} = input;
+    // TODO keyWord 没有太大必要
     let keyWord = column.map(object => object.name);
 
     return data.map(user => {
         let result = {};
+        // TODO forEach内可以使用箭头函数
         keyWord.forEach(function (value,index){
             result[value] = user[index];
         });
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,6 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
+    // TODO forEach内可以使用箭头函数
     sections.forEach(function(section,index){
         items.splice(section.index+index,0,section.content);
     });
